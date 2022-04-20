0ca34b0f011e0ec58ee009ce69fc6e2225318df1
Marina <marina.marqueso@outlook.com>
2022-04-19 22:43:58 -0300

    Feat: Add mechanic of header completed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,48 @@
+import { useNavigate } from 'react-router-dom';
+import { MdDeveloperMode } from 'react-icons/md';
+
 export default function Header(): JSX.Element {
+  const pages = [
+    {
+      name: 'Home',
+      route: '',
+    },
+    {
+      name: 'Sobre',
+      route: 'about',
+    },
+    {
+      name: 'Experiências',
+      route: 'experiences',
+    },
+    {
+      name: 'Projetos',
+      route: 'projects',
+    },
+    {
+      name: 'Contato',
+      route: 'contact',
+    },
+  ];
+  const navigate = useNavigate();
   return (
     <header>
-      header
+      <h1>
+        <MdDeveloperMode />
+        Marina Marques
+      </h1>
+      {
+        pages.map((element) => (
+          <button
+            type="button"
+            key={element.name}
+            className="pages-link"
+            onClick={() => navigate(`/${element.route}`)}
+          >
+            {element.name}
+          </button>
+        ))
+      }
     </header>
   );
 }
