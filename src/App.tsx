9ba89c7af922982569e61231b41e491ed0cc7cd5
Marina <marina.marqueso@outlook.com>
2022-04-19 22:06:50 -0300

    Feat: Create Pages and tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Experiences from './pages/Experiences';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/experiences" element={<Experiences />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </Router>
   );
