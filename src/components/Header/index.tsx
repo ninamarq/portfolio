ea104292399710e8eb2b087fc96e91d1a01bae03
Marina <marina.marqueso@outlook.com>
2022-04-26 16:26:54 -0300

    Feat: Finaliza header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AiFillHome } from 'react-icons/ai';
 import { BsFillPersonFill } from 'react-icons/bs';
 import { FaSuitcase, FaTelegramPlane } from 'react-icons/fa';
@@ -6,6 +6,8 @@ import { CgWebsite } from 'react-icons/cg';
 import './style.scss';
 
 export default function Header(): JSX.Element {
+  const { pathname } = useLocation();
+
   const pages = [
     {
       name: <AiFillHome />,
@@ -28,6 +30,7 @@ export default function Header(): JSX.Element {
       route: 'contact',
     },
   ];
+
   return (
     <header className="header-container">
       <ul className="link-list">
@@ -36,7 +39,10 @@ export default function Header(): JSX.Element {
             <li
               key={element.route}
             >
-              <Link className="pages-link" to={`/${element.route}`}>
+              <Link
+                className={pathname.split('/')[1] === element.route ? 'pages-link route' : 'pages-link'}
+                to={`/${element.route}`}
+              >
                 {element.name}
               </Link>
             </li>
