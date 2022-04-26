e0644256c6c2328a44d178ba5a07b68b213ca653
Marina <marina.marqueso@outlook.com>
2022-04-26 17:08:29 -0300

    Feat: Start home development, divide the page in two sides

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,18 @@
 import Header from '../../components/Header';
+import Profile from '../../assets/profile.jpeg';
+import './style.scss';
 
 export default function Home() {
   return (
-    <div>
+    <div className="page-container">
       <Header />
-      <h1>Home</h1>
+      <div className="left-container">
+        <h2>Hello There!</h2>
+        <h1>Marina Marques</h1>
+      </div>
+      <div className="right-container">
+        <img src={Profile} alt="Marina Marques" />
+      </div>
     </div>
   );
 }
