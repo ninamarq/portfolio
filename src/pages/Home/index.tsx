f78f948048f120ec8abc297bcbe5d01d3eb1c976
Marina <marina.marqueso@outlook.com>
2022-04-26 18:37:45 -0300

    Feat: Create content for both sides

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,8 +7,27 @@ export default function Home() {
     <div className="page-container">
       <Header />
       <div className="left-container">
-        <h2>Hello There!</h2>
-        <h1>Marina Marques</h1>
+        <div>
+          <h2>Hello there!</h2>
+          <h1>
+            I'm
+            {' '}
+            Marina
+            {' '}
+            <span>Marques</span>
+          </h1>
+          <h3>Full-Stack Developer</h3>
+        </div>
+        <p>
+          I'm from Brazil, Minas Gerais, the state famous for it's famous
+          {' '}
+          <a href="https://www.youtube.com/watch?v=IOb5uric4n0">🧀</a>
+          {' '}
+          "cheese-bread".
+          A new student and a new worker at the great Web Universe,
+          a coffee lover ☕ and a happy developer.
+        </p>
+        <button type="button">About Me</button>
       </div>
       <div className="right-container">
         <img src={Profile} alt="Marina Marques" />
