52fe6f4b89f1e786d1be6c62c1c7c5b2689cb4ec
Marina <marina.marqueso@outlook.com>
2022-04-26 22:28:28 -0300

    Feat: Work at skills's style

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import {
   SiCss3, SiHtml5, SiJavascript, SiReact, SiSass, SiTypescript,
 } from 'react-icons/si';
@@ -6,34 +7,42 @@ import Header from '../../components/Header';
 import './style.scss';
 
 export default function About() {
+  const navigate = useNavigate();
   const technologies = [
     {
       name: 'HTML',
       logo: <SiHtml5 />,
+      site: 'https://devdocs.io/html/',
     },
     {
       name: 'CSS',
       logo: <SiCss3 />,
+      site: 'https://devdocs.io/css/',
     },
     {
       name: 'Sass',
       logo: <SiSass />,
+      site: 'https://sass-lang.com/documentation',
     },
     {
       name: 'Typescript',
       logo: <SiTypescript />,
+      site: 'https://www.typescriptlang.org/',
     },
     {
       name: 'Javascript',
       logo: <SiJavascript />,
+      site: 'https://devdocs.io/javascript/',
     },
     {
       name: 'Node',
       logo: <GrNode />,
+      site: 'https://nodejs.org/en/docs/',
     },
     {
       name: 'React',
       logo: <SiReact />,
+      site: 'https://reactjs.org/docs/hello-world.html',
     },
   ];
 
@@ -59,7 +68,14 @@ export default function About() {
           </p>
           <p>
             Since July of 2021, I'm a web development student at
-            <span> Trybe</span>
+            <a
+              href="https://www.betrybe.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {' '}
+              Trybe
+            </a>
             . Always building
             projects monthly and taking classes everyday.
           </p>
@@ -69,15 +85,24 @@ export default function About() {
             Main
             <span> Skills</span>
           </h2>
-          <div>
+          <ul>
             {
               technologies.map((element) => (
-                <div key={element.name} className="technology-card">
-                  {element.logo}
-                </div>
+                <li
+                  key={element.name}
+                  className="technology-card"
+                >
+                  <a
+                    href={element.site}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {element.logo}
+                  </a>
+                </li>
               ))
             }
-          </div>
+          </ul>
         </section>
       </main>
     </div>
