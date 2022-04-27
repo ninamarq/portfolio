68c8c2ed3ac39afca5fb2ca1f31032c53611b374
Marina <marina.marqueso@outlook.com>
2022-04-27 11:50:32 -0300

    Feat: Add Style and prepare to create background

--- src/pages/Experiences/index.tsx
+++ src/pages/Experiences/index.tsx
@@ -1,37 +1,43 @@
 import { HiAcademicCap } from 'react-icons/hi';
 import { TiDeviceLaptop } from 'react-icons/ti';
+import './style.scss';
 
 export default function Experiences() {
   return (
-    <div>
-      <h1>Experiences</h1>
-      <section>
-        <h2>
-          Education
-          <HiAcademicCap />
-        </h2>
-        <div>
-          <h3>February 2019 - July 2023</h3>
-          <h2>Química Tecnológica</h2>
-          <p>Universidade Federal de Minas Gerais</p>
-        </div>
-        <div>
-          <h3>July 2020 - August 2022</h3>
-          <h2>Web Development | Full-Stack</h2>
-          <p>Trybe</p>
-        </div>
-      </section>
-      <section>
-        <h2>
-          Jobs
-          <TiDeviceLaptop />
-        </h2>
-        <div>
-          <h3>February 2022 - Current</h3>
-          <h2>Front-End Developer</h2>
-          <a href="https://dopster.io/" target="_blank" rel="noreferrer">Dopster.io</a>
-        </div>
-      </section>
+    <div className="experience-container">
+      <h1>
+        Experi
+        <span>ences</span>
+      </h1>
+      <div className="experiences">
+        <section className="experiences-cards">
+          <h2>
+            Education
+            <HiAcademicCap />
+          </h2>
+          <div>
+            <h4>February 2019 - July 2023</h4>
+            <h3>Química Tecnológica</h3>
+            <p>Universidade Federal de Minas Gerais</p>
+          </div>
+          <div>
+            <h4>July 2020 - August 2022</h4>
+            <h3>Web Development | Full-Stack</h3>
+            <p>Trybe</p>
+          </div>
+        </section>
+        <section className="experiences-cards">
+          <h2>
+            Jobs
+            <TiDeviceLaptop />
+          </h2>
+          <div>
+            <h4>February 2022 - Current</h4>
+            <h3>Front-End Developer</h3>
+            <p>Dopster</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
